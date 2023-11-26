70dae5e02bf41461da61ed96dddc1fd04edada7a
Julien L <julienlenaz96@gmail.com>
2023-11-25 22:15:18 -0500

    updated models

--- AnimalShelterAPI/Models/Animals.cs
+++ AnimalShelterAPI/Models/Animals.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelterApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
-    public int Age { get; set; } 
+    [Required]
+    [Range(0, 100, ErrorMessage = "Age must be between 0 and 100.")]
+    public int Age { get; set; }
   }
 }
