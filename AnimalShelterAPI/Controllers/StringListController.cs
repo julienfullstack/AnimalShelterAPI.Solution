abd09d4d612fc0e12bf0f342d50d80cacd0ab435
Julien L <julienlenaz96@gmail.com>
2023-12-11 15:18:14 -0500

    Updated StringList controller

--- AnimalShelterAPI/Controllers/StringListController.cs
+++ AnimalShelterAPI/Controllers/StringListController.cs
@@ -1,21 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelterApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AnimalShelterApi.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class AnimalController : ControllerBase
+  public class StringListController : Controller
   {
     private readonly AnimalShelterApiContext _db;
 
-    public AnimalController(AnimalShelterApiContext db)
+    public StringListController(AnimalShelterApiContext db)
     {
       _db = db;
     }
 
-
+    
     [Route("api/v{version:apiVersion}/StringList")]
     [ApiVersion("1.0")]
     public async Task<List<Animal>> GetV1(string species)
